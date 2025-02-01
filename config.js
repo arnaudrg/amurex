87dc3e4fb23b0a52f65b815a7dbd3219c6b25427
arnaudrg <arnaudrobertgorsse@gmail.com>
2025-02-01 15:16:52 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const AMUREX_CONFIG = {
+  BASE_URL_BACKEND: "http://localhost:8080",  // Your backend server URL
+  BASE_URL_WEB: "http://localhost:8080",      // Your web server URL
+  ANALYTICS_ENABLED: true                     // Set to false to disable tracking
+};
+window.AMUREX_CONFIG = AMUREX_CONFIG;
